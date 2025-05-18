6e6afef4a11bb28081f3be3411185d39f435bff9
Bollapu Sandeep kumar reddy <bollapusandeepkumarreddy@Bollapus-MacBook-Pro.local>
2025-05-18 16:08:51 -0500

    added command to run locally

--- Introduction-to-fastapi/helloWorld-fastapi.py
+++ Introduction-to-fastapi/helloWorld-fastapi.py
@@ -7,4 +7,6 @@ def read_root():
     return {"hello": "world"}
 
 
+# Command to run FastAPI locally- uvicorn helloWorld-fastapi:app --reload 
+
 # http://127.0.0.1:8000/docs - for getting documentation of FastAPI
